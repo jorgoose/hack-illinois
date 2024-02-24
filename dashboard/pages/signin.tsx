cbfe0fc6ee3b55803e94849c28eb8c6fd511600b
Kyle Goben <kygoben@gmail.com>
2024-02-24 10:52:29 -0600

    get repos

--- dashboard/pages/signin.tsx
+++ dashboard/pages/signin.tsx
@@ -1,17 +1,26 @@
 // pages/signin.js
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import { Octokit } from "@octokit/core";
 
 export default function SignIn() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   const generateToken = async () => {
-    try {
-      const response = await axios.post('/api/generate-token');
-      setToken(response.data.token);
-    } catch (error) {
-      console.error('Error generating token:', error);
-    }
+    const { Octokit } = require("@octokit/core");
+
+    const octokit = new Octokit({
+      auth: "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",
+    });
+
+    console.log(
+      await octokit.request("GET /user/repos", {
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+    );
+    console.log("octokit", octokit);
   };
 
   return (
