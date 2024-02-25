28ab790c214c7cedfadc7830e6e7ee9489469f63
Chris Tague <cstague6401@gmail.com>
2024-02-24 21:28:11 -0600

    lambda prep again

--- backend/pack_lambda.sh
+++ backend/pack_lambda.sh
@@ -3,3 +3,10 @@ cp lambda_function.py lambda_dir
 cp test_walker.py lambda_dir
 cp requirements.txt lambda_dir
 cp proompt.py lambda_dir
+cp -r context lambda_dir
+cd lambda_dir
+pip install -r requirements.txt -t .
+zip fuzz_guard.zip *
+mv fuzz_guard.zip ../
+cd ..
+rm -rf lambda_dir
